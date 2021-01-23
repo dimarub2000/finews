8667ba1d2e2bcb3df8e05927543904835f8dfb59
godlikevaslas <vmshelkovnikov@gmail.com>
2021-01-23 18:56:15 +0300

    finam.ru parser

--- parser/html/html_parser.py
+++ parser/html/html_parser.py
@@ -8,21 +8,36 @@ class HtmlParser(object):
 
     def get_html_file(self):
         resp = requests.get(self.url)
-        soup = BeautifulSoup(resp.text, 'lxml')
-        self.parse(soup)
+        soup = BeautifulSoup(resp.text, 'html.parser')
+        return self.parse(soup)
 
     def parse(self, soup):
         pass
 
 
-# example
-class BloombergParser(HtmlParser):
+class FinamParser(HtmlParser):
     def parse(self, soup):
-        text_file = open("html.txt", "w")
-        n = text_file.write(str(soup))
-        text_file.close()
+        docs = []
+        for news in soup.find_all('td', class_='ntitle bdotline', limit=5):
+            for link in news.find_all('a'):
+                new_url = 'https://www.finam.ru' + link.get('href')
+                finam_parser = FinamCoreParser(new_url)
+                res = finam_parser.get_html_file()
+                if res is not None:
+                    docs.append(res)
+        return docs
 
 
-url = 'https://www.bloomberg.com/europe'
-parser = BloombergParser(url)
-parser.get_html_file()
+class FinamCoreParser(HtmlParser):  # parsing refs from Finam main page
+    def parse(self, soup):
+        for text in soup.find_all('div', class_='handmade mid f-newsitem-text', limit=1):
+            return text.get_text()
+
+
+# example
+url = 'https://www.finam.ru/analysis/nslent/'
+parser = FinamParser(url)
+res = parser.get_html_file()
+for elem in res:
+    print(elem)
+    print('----------------------------------')
