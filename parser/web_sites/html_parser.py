210a28ab2ed4728e3edfdbaafc2ce3174f1295c5
dimarub2000 <dimarub2000@gmail.com>
2021-01-26 19:03:05 +0300

    add json logic

--- parser/web_sites/html_parser.py
+++ parser/web_sites/html_parser.py
@@ -1,19 +1,21 @@
+import json
 import requests
-from bs4 import BeautifulSoup
 import dateparser
+from bs4 import BeautifulSoup
 
 
 class HtmlParser(object):
+    """Parses single html page to JSON"""
     def __init__(self, url):
         self.url = url
 
-    def get_data(self):
+    def get_data(self) -> str:
         resp = requests.get(self.url)
-        soup = BeautifulSoup(resp.text, 'web_sites.parser')
+        soup = BeautifulSoup(resp.text, 'html.parser')
         return self.parse(soup)
 
-    def parse(self, soup):
-        pass
+    def parse(self, soup) -> str:
+        return json.dumps({'Parse error'})
 
 
 class FinamParser(HtmlParser):
@@ -23,17 +25,17 @@ class FinamParser(HtmlParser):
             for link in news.find_all('a', class_='f-fake-url', limit=1):
                 new_url = 'https://www.finam.ru' + link.get('href')
                 finam_parser = FinamCoreParser(new_url)
-                res = finam_parser.get_data()
+                res = json.loads(finam_parser.get_data())
                 if res is not None:
                     docs.append(res)
-        return docs
+        return json.dumps(docs)
 
 
-class FinamCoreParser(HtmlParser):  # parsing refs from Finam main page
+class FinamCoreParser(HtmlParser):
     def parse(self, soup):
-        text = soup.find('div', class_='handmade mid f-newsitem-text')
+        text = soup.find('div', class_='handmade mid f-newsitem-text').get_text()
         date = dateparser.parse(soup.find('div', class_='sm lightgrey mb05 mt15').get_text()[:17]).strftime("%m/%d/%Y, %H:%M:%S")
-        return {'text': text.get_text(), 'source': 'Finam', 'date': date}
+        return json.dumps({'text': text, 'date': date, 'source': 'Finam'})
 
 
 class BCSParser(HtmlParser):
@@ -42,23 +44,28 @@ class BCSParser(HtmlParser):
         for news in soup.find_all('a', class_='feed-item__head', limit=5):
             new_url = 'https://www.bcs-express.ru' + news.get('href')
             bcs_parser = BCSCoreParser(new_url)
-            res = bcs_parser.get_data()
+            res = json.loads(bcs_parser.get_data())
             if res is not None:
                 docs.append(res)
-        return docs
+        return json.dumps(docs)
 
 
 class BCSCoreParser(HtmlParser):
     def parse(self, soup):
-        text = soup.find('div', class_='article__text')
+        text = soup.find('div', class_='article__text').get_text()
         date = dateparser.parse(soup.find('div', class_='article__info-time').get_text().strip()).strftime("%m/%d/%Y, %H:%M:%S")
-        return {'text': text.get_text(), 'date': date, 'source': 'BCS'}
+        return json.dumps({'text': text, 'date': date, 'source': 'BCS'})
 
 
-# example
-url = 'https://www.finam.ru/analysis/nslent/'
-parser = FinamParser(url)
-res = parser.get_data()
-for elem in res:
-    print(elem)
-    print('----------------------------------')
+def main():
+    url = 'https://www.finam.ru/analysis/nslent/'
+    parser = FinamParser(url)
+    res = json.loads(parser.get_data())
+    for elem in res:
+        print(elem)
+        print('----------------------------------')
+
+
+if __name__ == '__main__':
+    main()
+
