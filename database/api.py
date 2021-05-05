e2628477a855148d1b0fa63a669a6e0226e89484
dimarub2000 <dimarub2000@gmail.com>
2021-05-05 22:06:16 +0300

    add top method

--- database/api.py
+++ database/api.py
@@ -1,60 +1,78 @@
 import json
-import logging
 
 from flask import request
+from sqlalchemy import desc
 from database import app, db
-from marshmallow import Schema, fields, post_load
-
-logger = logging.getLogger(__name__)
 
 
 class News(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True)
-    content = db.Column(db.String(1000))
     time = db.Column(db.String(20))
     link = db.Column(db.String(64))
-    tags = db.Column(db.String(12), nullable=True)
+    tags = db.relationship('Tag', backref='news')
     source = db.Column(db.String(24))
+    content = db.Column(db.String(1000))
+
+    def to_dict(self):
+        res = dict()
+        res['id'] = self.id
+        res['content'] = self.content
+        res['time'] = self.time
+        res['link'] = self.link
+        res['tags'] = [elem.tag for elem in self.tags]
+        res['source'] = self.source
+        return res
+
+    @staticmethod
+    def news_to_dict(news):
+        return list(map(lambda x: x.to_dict(), news))
 
 
-class NewsSchema(Schema):
-    id = fields.Int()
-    content = fields.Str()
-    time = fields.Str()
-    link = fields.Str()
-    tags = fields.Str()
-    source = fields.Str()
-
-    @post_load
-    def make_user(self, data, **kwargs):
-        return News(**data)
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tag = db.Column(db.String(12), nullable=True)
+    news_id = db.Column(db.Integer, db.ForeignKey('news.id'), nullable=False)
 
 
-news_schema = NewsSchema()
-many_news_schema = NewsSchema(many=True)
+db.create_all()
 
 
 @app.route('/news', methods=['POST'])
 def add_news():
     data = request.get_json()
-    logger.critical("data {}".format(data))
+
     for news in data:
-        db.session.add(News(content=news['text'],
-                            time=news['time'],
-                            link=news['link'],
-                            tags=news.get('tags'),
-                            source=news.get('source')))
+        cur_news = News(
+            content=news['text'],
+            time=news['time'],
+            link=news['link'],
+            source=news.get('source')
+        )
+        db.session.add(cur_news)
+        for tag in news.get('tags', []):
+            db.session.add(Tag(tag=tag, news=cur_news))
+
     db.session.commit()
-    return 'OK'
+    return 'OK\n'
 
 
 @app.route('/news', methods=['GET'])
 def get_news():
     news = News.query.all()
-    return json.dumps(many_news_schema.dump(news, many=True))
+    return json.dumps(News.news_to_dict(news))
 
 
-if __name__ == '__main__':
-    logger.critical("Creating all")
-    db.create_all()
+@app.route('/top', methods=['GET'])
+def get_top():
+    tag = request.args.get('tag')
+    limit = request.args.get('limit', default=1, type=int)
+    if tag is None:
+        news = News.query.order_by(desc(News.id)).limit(limit).all()
+    else:
+        news = News.query.filter(News.tags.any(tag=tag)).order_by(desc(News.id)).limit(limit).all()
+
+    return json.dumps(News.news_to_dict(news))
+
+
+if __name__ == "__main__":
     app.run()
