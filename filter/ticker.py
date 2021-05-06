cced818fc5a25af75de000b5c12f0c1592f03023
godlikevaslas <vmshelkovnikov@gmail.com>
2021-05-06 21:10:32 +0300

    redundant files deleted

--- filter/ticker.py
+++ filter/ticker.py
@@ -1,4 +1,6 @@
 def parse_ticker(text, start):
+    if text[start].isdigit():
+        return None
     ticker = ""
     while text[start].isspace() or text[start].isdigit():
         ticker += text[start]
@@ -11,6 +13,8 @@ def find_tickers(text):
     pos = text.find('$')
     while pos != -1:
         pos += 1
-        tickers.append(parse_ticker(text, pos))
+        ticker = parse_ticker(text, pos)
+        if ticker is not None:
+            tickers.append(ticker)
         pos = text.find('$', pos)
     return tickers
