f9ca2c4e4da8677f08dfcc7aa96d55c79263dc70
Ruslan Kamalov <pllpi@sandbox-633380738>
2021-05-06 21:02:54 +0300

    bot whithout subscribtion

--- tg_bot/tg_bot_finews.py
+++ tg_bot/tg_bot_finews.py
@@ -13,22 +13,31 @@ def get_text_messages(message):
     global state
     if message.text == "/commands":
         markup = types.ReplyKeyboardMarkup()
-        itembtn_top_news = types.KeyboardButton('top_news')
-        itembtn_ticker = types.KeyboardButton('ticker')
+        itembtn_top_news = types.KeyboardButton('top news')
+        itembtn_ticker = types.KeyboardButton('tickers')
         itembtn_subscribe = types.KeyboardButton('subscribe')
         markup.row(itembtn_top_news, itembtn_ticker)
         markup.row(itembtn_subscribe)
         bot.send_message(message.from_user.id, "Выбери одну из команд на панели ниже", reply_markup=markup)
     elif message.text == "/help":
         bot.send_message(message.from_user.id, "Привет! Напиши '/commands' и тебе выведутся все доступные на данный момент функции данного бота.")
-    elif message.text == "top_news":
+    elif message.text == "top news":
         state = 1
-        bot.send_message(message.from_user.id, "Впиши тикер компании которая тебя интересует (если интересуют новости по всем компаниям сразу напиши -). Следующие тикеры доступны на данный момент:")
+        bot.send_message(message.from_user.id, "Впиши тикер компании которая тебя интересует (если интересуют новости по всем компаниям сразу напиши -).")
 
         bot.register_next_step_handler(message, get_tag)
 
-    elif message.text == "ticker":
-        pass
+    elif message.text == "tickers":
+        tickers_list = list(map(lambda x: '$' + x, requests.get('http://127.0.0.1:5000/tags').json()))
+        ans = ''
+        first = True
+        for item in tickers_list:
+            if not first:
+                ans += ', '
+            else:
+                first = False
+            ans += item
+        bot.send_message(message.from_user.id, ans)
     elif message.text == "subscribe":
         pass
     elif state == 0:
@@ -37,7 +46,7 @@ def get_text_messages(message):
 def get_tag(message):
     global user_tag
 
-    user_tag = message.text
+    user_tag = message.text.replace('$', '').upper()
     bot.send_message(message.from_user.id, "Напиши максимальное число последних новостей по этой компании, которые тебя интересуют")
     bot.register_next_step_handler(message, get_limit)
 
@@ -50,12 +59,17 @@ def get_limit(message):
         data = list(requests.get('http://127.0.0.1:5000/top?tag={}&limit={}'.format(user_tag, user_limit)).json())
 
     for item in data:
+        bot.send_message(message.from_user.id, "Новость по компании: {},"
+        " время публикации: {}, ссылка на источник: {}".format(item['tags'], item['time'], item['link']),
+         disable_web_page_preview=True)
         bot.send_message(message.from_user.id, item['content'])
+        if (data.index(item) + 1 != len(data)):
+            bot.send_message(message.from_user.id, "=================================================")
 
     if len(data) == 0:
         bot.send_message(message.from_user.id, "Ничего не найдено по такому запросу, либо не валидный лимит"
-                " (попробуйте любое положительное число), либо нет такого тикера на данный момен (ниже"
-                " представлены все доступные на данный момент тикеры)")
+                " (попробуйте любое положительное число), либо нет такого тикера на данный момен ("
+                " чтобы посмотреть доступные на данный момент тикеры, напиши мне команду: tickers)")
 
     state = 0
 
