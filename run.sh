5767559c0f8d80740b49c3d4e768424ea5ec565c
godlikevaslas <vmshelkovnikov@gmail.com>
2021-05-07 17:13:01 +0300

    better script

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+#! /bin/bash
+
+lsof -i tcp:4000 | grep -v PID | awk '{print $2}' | xargs kill
+lsof -i tcp:5000 | grep -v PID | awk '{print $2}' | xargs kill
 export FLASK_APP=api.py
 cd filter
 flask run --port=4000 > filter.log &
