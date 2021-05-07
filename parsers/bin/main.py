2d56e8ac137afd155d9e0ce22d74dd6742609ada
Ruslan Kamalov <pllpi@MacBook-Pro-62.local>
2021-05-07 17:23:41 +0300

    register tg

--- parsers/bin/main.py
+++ parsers/bin/main.py
@@ -28,24 +28,24 @@ def init_sources() -> List[lib_parser.Source]:
             None,
             'html'
         ),
-        # lib_parser.Source(
-        #     lib_tg.TgParser('https://t.me/Full_Time_Trading', 1),
-        #     'Full Time Trading',
-        #     None,
-        #     'tg'
-        # ),
-        # lib_parser.Source(
-        #     lib_tg.TgParser('https://t.me/stock_and_news', 1),
-        #     'Financial Times',
-        #     None,
-        #     'tg'
-        # ),
-        # lib_parser.Source(
-        #     lib_tg.TgParser('https://t.me/mtwits', 1),
-        #     'Market Twits',
-        #     None,
-        #     'tg'
-        # ),
+        lib_parser.Source(
+            lib_tg.TgParser('https://t.me/Full_Time_Trading', 1),
+            'Full Time Trading',
+            None,
+            'tg'
+        ),
+        lib_parser.Source(
+            lib_tg.TgParser('https://t.me/stock_and_news', 1),
+            'Financial Times',
+            None,
+            'tg'
+        ),
+        lib_parser.Source(
+            lib_tg.TgParser('https://t.me/mtwits', 1),
+            'Market Twits',
+            None,
+            'tg'
+        ),
     ]
     return sources
 
