808dd1ba9f921655528c40395d40118b1701dcc3
dimarub2000 <dimarub2000@gmail.com>
2021-05-07 20:12:39 +0300

    new run

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 
-lsof -i tcp:4000 | grep -v PID | awk '{print $2}' | xargs kill
 lsof -i tcp:5000 | grep -v PID | awk '{print $2}' | xargs kill
-cd filter
-flask run --port=4000 > filter.out 2> filter.err & 
-cd ../database
+lsof -i tcp:9001 | grep -v PID | awk '{print $2}' | xargs kill
+lsof -i tcp:9002 | grep -v PID | awk '{print $2}' | xargs kill
+
+cd database
 flask run --port=5000 > database.out 2> database.err &
-cd ../
+cd ../filter
+flask run --port=9001 > filter.out 2> filter.err &
+cd ../search
+flask run --port=9002 > search.out 2> search.err &
+cd ..
 PYTHONPATH=$(pwd) python3 parsers/bin/main.py
