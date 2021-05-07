ef2539ea14eec147f3ec1a4cf324a87cc011d097
godlikevaslas <vmshelkovnikov@gmail.com>
2021-05-07 18:34:52 +0300

    style fixes

--- filter/ticker_parser.py
+++ filter/ticker_parser.py
@@ -1,9 +1,9 @@
 import string
 
 
-class TickerParser(object):
-    def __init__(self, ticker_symbol='$'):
-        self.ticker_symbol = ticker_symbol
+class TagsParser(object):
+    def __init__(self, tag_symbol='$'):
+        self.ticker_symbol = tag_symbol
 
     def __parse_ticker(self, text, start):
         if text[start].isdigit():
@@ -14,13 +14,13 @@ class TickerParser(object):
             start += 1
         return ticker
 
-    def find_tickers(self, text):
-        tickers = set()
+    def find_tags(self, text):
+        tags = set()
         pos = text.find('$')
         while pos != -1:
             pos += 1
             ticker = self.__parse_ticker(text, pos)
             if ticker is not None:
-                tickers.add(ticker)
+                tags.add(ticker)
             pos = text.find('$', pos)
-        return list(tickers)
+        return list(tags)
--- tg_bot/tg_bot_finews.py
+++ tg_bot/tg_bot_finews.py
@@ -8,6 +8,7 @@ user_tag = ''
 user_limit = ''
 state = 0
 
+
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     global state
@@ -20,10 +21,14 @@ def get_text_messages(message):
         markup.row(itembtn_subscribe)
         bot.send_message(message.from_user.id, "Выбери одну из команд на панели ниже", reply_markup=markup)
     elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Привет! Напиши '/commands' и тебе выведутся все доступные на данный момент функции данного бота.")
+        bot.send_message(message.from_user.id,
+                         "Привет! Напиши '/commands' и тебе выведутся все доступные на данный момент функции данного "
+                         "бота.")
     elif message.text == "top news":
         state = 1
-        bot.send_message(message.from_user.id, "Впиши тикер компании которая тебя интересует (если интересуют новости по всем компаниям сразу напиши -).")
+        bot.send_message(message.from_user.id,
+                         "Впиши тикер компании которая тебя интересует (если интересуют новости по всем компаниям "
+                         "сразу напиши -).")
 
         bot.register_next_step_handler(message, get_tag)
 
@@ -43,33 +48,40 @@ def get_text_messages(message):
     elif state == 0:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши '/help'.")
 
+
 def get_tag(message):
     global user_tag
 
     user_tag = message.text.replace('$', '').upper()
-    bot.send_message(message.from_user.id, "Напиши максимальное число последних новостей по этой компании, которые тебя интересуют")
+    bot.send_message(message.from_user.id,
+                     "Напиши максимальное число последних новостей по этой компании, которые тебя интересуют")
     bot.register_next_step_handler(message, get_limit)
+
 
 def get_limit(message):
     global user_limit, state
     user_limit = message.text
-    if (user_tag == '-'):
+    if user_tag == '-':
         data = list(requests.get('http://127.0.0.1:5000/top?limit={}'.format(user_limit)).json())
     else:
         data = list(requests.get('http://127.0.0.1:5000/top?tag={}&limit={}'.format(user_tag, user_limit)).json())
 
     for item in data:
         bot.send_message(message.from_user.id, "Новость по компании: {},"
-        " время публикации: {}, ссылка на источник: {}".format(item['tags'], item['time'], item['link']),
-         disable_web_page_preview=True)
+                                               " время публикации: {}, ссылка на источник: {}".format(item['tags'],
+                                                                                                      item['time'],
+                                                                                                      item['link']),
+                         disable_web_page_preview=True)
         bot.send_message(message.from_user.id, item['content'])
-        if (data.index(item) + 1 != len(data)):
+        if data.index(item) + 1 != len(data):
             bot.send_message(message.from_user.id, "=================================================")
 
     if len(data) == 0:
         bot.send_message(message.from_user.id, "Ничего не найдено по такому запросу, либо не валидный лимит"
-                " (попробуйте любое положительное число), либо нет такого тикера на данный момен ("
-                " чтобы посмотреть доступные на данный момент тикеры, напиши мне команду: tickers)")
+                                               "(попробуйте любое положительное число), либо нет такого тикера на "
+                                               "данный момен ( "
+                                               "чтобы посмотреть доступные на данный момент тикеры, напиши мне "
+                                               "команду: tickers)")
 
     state = 0
 
